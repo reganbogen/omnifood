8020355f73faf645582881e03e59559a6cbd9d74
Regan Layman <regan.layman@gmail.com>
2018-09-04 15:29:47 -0400

    added offset so the text becomes visible more quickly

--- resources/js/script.js
+++ resources/js/script.js
@@ -44,6 +44,8 @@ $(document).ready(function() {
   
   $('.js--wp-1').waypoint(function(direction){
     $('.js--wp-1').addClass('animated fadeIn');
-  })
+  }, {
+      offset: '50%'
+  });
   
 });
