19354ed809026957ac69cf45ec4c09288cebc085
Regan Layman <regan.layman@gmail.com>
2018-09-04 13:37:34 -0400

    got reference information from guide on waypoint website. added if/else statement to make sticky nav appear if you get to a certain point, class js--section-features, and added class 'sticky.' so i removed class from main nav in index.html

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   
+  $('.js--section-features').waypoints(function(direction){
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      
+    }
+  })
 
   
 });
